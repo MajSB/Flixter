251acdcf65933b3d5ff1ec4a48aae3b155e6a766
Majda <majda.saidi22@gmail.com>
2016-02-18 14:11:06 +0000

    Moving the new section form into a modal

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,10 @@ Rails.application.routes.draw do
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :lessons, only: [:update]
-    resources :sections, only: [:update]
-    resources :sections, only: [] do
+    resources :sections, only: [:update] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-      resources :sections, only: [:new, :create]
     end
   end
 
